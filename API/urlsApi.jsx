96bd7456541dd9f9c50f8016a5110b8c767f2140
Eduardo Aguilar <es123y123@gmail.com>
2023-10-19 12:34:41 -0500

    Cam

--- API/urlsApi.jsx
+++ API/urlsApi.jsx
@@ -1,6 +1,6 @@
 //export const base = 'https://api-montacargas-production.up.railway.app/';
 // export const base = 'http://192.168.1.232:8080/api';
- export const base = 'http://161.132.180.242:65006/api'; 
+ export const base = 'http://161.132.180.242:65006/api/api'; 
 export const carrilesURL = `${base}/carriles`;
 //export const carrilesURL = `https://api-montacargas-production.up.railway.app/api/carriles`;
 export const asignarMont = `${carrilesURL}/asignarMontacargas/`
