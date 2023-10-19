077460af2121d05529df7c90f599471aeb32bf66
Eduardo Aguilar <es123y123@gmail.com>
2023-10-19 11:48:23 -0500

    CambioAPI

--- API/urlsApi.jsx
+++ API/urlsApi.jsx
@@ -1,7 +1,8 @@
 //export const base = 'https://api-montacargas-production.up.railway.app/';
-//export const base = 'http://192.168.1.232:8080/api';
-//export const carrilesURL = `${base}/carriles`;
-export const carrilesURL = `https://api-montacargas-production.up.railway.app/api/carriles`;
+// export const base = 'http://192.168.1.232:8080/api';
+ export const base = 'http://161.132.180.242:65006/api';
+export const carrilesURL = `${base}/carriles`;
+//export const carrilesURL = `https://api-montacargas-production.up.railway.app/api/carriles`;
 export const asignarMont = `${carrilesURL}/asignarMontacargas/`
 export const salidaConductorURL = `${carrilesURL}/salidaConductor/`
 export const finAuxiliarURL = `${carrilesURL}/finAuxiliar/`
