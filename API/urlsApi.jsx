0991fa028e563529a5aa84aff91abdf54f776578
Eduardo Aguilar <es123y123@gmail.com>
2023-12-05 18:00:55 -0500

    Remoto

--- API/urlsApi.jsx
+++ API/urlsApi.jsx
@@ -2,7 +2,9 @@
 //export const base = 'http://192.168.1.202:65006/api';
 // export const base = '161.132.180.242:65006/api/api';
 
-export const base = 'https://samloto.com:65006/api';
+// export const base = 'https://samloto.com:65006/api';
+
+export const base = 'https://montacargas-api-production.up.railway.app/api';
 
 export const carrilesURL = `${base}/carriles`;
 //export const carrilesURL = `https://api-montacargas-production.up.railway.app/api/carriles`;
